6e1df9ff64d0cab20f8aa643731c51d87f1ddacd
dngrhm <dngrhm@users.noreply.github.com>
2022-06-21 14:12:59 -0400

    :bug: fix: Adds purple swatches as primary color in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#ecebff',
+          100: '#c7c2ff',
+          200: '#a299ff',
+          300: '#8f85ff',
+          400: '#7c70ff',
+          500: '#695cff',
+          600: '#5546ff',
+          700: '#311fff',
+          800: '#1100cc',
+          900: '#0c008f',
+        },
+      },
+    },
   },
   plugins: [],
 }
